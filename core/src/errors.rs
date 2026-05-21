0960c434fec6045ffdd622cc18ed3dc1ffe2a5e4
Shiyo1101 <yoshiki1011abc@gmail.com>
2026-05-21 22:55:29 +0900

    ♻️ BasicError列挙型にエラーメッセージ対応表を追加し、エラー処理を最適化

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -3,6 +3,9 @@
 //! 内部処理は依然として `Machine.err: u8` のフラグを伝搬させているが、
 //! 公開 API 境界 (`exec_line` など) では [`BasicError`] を `Result` で
 //! 返す。
+//!
+//! コード番号 ⇔ 列挙子 ⇔ メッセージ文言の対応は [`ERROR_TABLE`] を唯一
+//! の真実とし、他のメソッドは全てこれを参照する。
 
 use std::fmt;
 
@@ -24,61 +27,77 @@ pub enum BasicError {
     Break,
 }
 
+/// `(コード番号, 列挙子, IchigoJam 標準メッセージ)` の対応表。
+/// インデックス 0 は「エラー無し」を示すダミー (空文字列)。
+const ERROR_TABLE: &[(u8, Option<BasicError>, &str)] = &[
+    (0, None, ""),
+    (1, Some(BasicError::SyntaxError), "Syntax error"),
+    (2, Some(BasicError::OutOfMemory), "Out of memory"),
+    (3, Some(BasicError::StackOverflow), "Stack overflow"),
+    (4, Some(BasicError::NotMatch), "Not match"),
+    (5, Some(BasicError::UndefinedLine), "Line error"),
+    (6, Some(BasicError::DivideByZero), "Divide by 0"),
+    (7, Some(BasicError::IndexOutOfRange), "Index out of range"),
+    (8, Some(BasicError::FileError), "File error"),
+    (9, Some(BasicError::SegmentationFault), "Segmentation Fault"),
+    (10, Some(BasicError::ComplexExpression), "Complex expression"),
+    (11, Some(BasicError::IllegalArgument), "Illegal argument"),
+    (12, Some(BasicError::Break), "Break"),
+];
+
 impl BasicError {
     /// 元 C 実装と同じ番号 (1..=12)。`Machine.err` の値と対応。
     pub const fn code(self) -> u8 {
-        match self {
-            BasicError::SyntaxError => 1,
-            BasicError::OutOfMemory => 2,
-            BasicError::StackOverflow => 3,
-            BasicError::NotMatch => 4,
-            BasicError::UndefinedLine => 5,
-            BasicError::DivideByZero => 6,
-            BasicError::IndexOutOfRange => 7,
-            BasicError::FileError => 8,
-            BasicError::SegmentationFault => 9,
-            BasicError::ComplexExpression => 10,
-            BasicError::IllegalArgument => 11,
-            BasicError::Break => 12,
+        // const 文脈で iter を使えないため線形検索を手で展開
+        let mut i = 1;
+        while i < ERROR_TABLE.len() {
+            if let (n, Some(e), _) = ERROR_TABLE[i] {
+                if matches_variant(e, self) {
+                    return n;
+                }
+            }
+            i += 1;
         }
+        0
     }
 
     /// 元 C 実装の番号からエラーへ復元。`0` なら `None` (エラー無し)。
     pub const fn from_code(code: u8) -> Option<Self> {
-        Some(match code {
-            1 => BasicError::SyntaxError,
-            2 => BasicError::OutOfMemory,
-            3 => BasicError::StackOverflow,
-            4 => BasicError::NotMatch,
-            5 => BasicError::UndefinedLine,
-            6 => BasicError::DivideByZero,
-            7 => BasicError::IndexOutOfRange,
-            8 => BasicError::FileError,
-            9 => BasicError::SegmentationFault,
-            10 => BasicError::ComplexExpression,
-            11 => BasicError::IllegalArgument,
-            12 => BasicError::Break,
-            _ => return None,
-        })
+        let mut i = 1;
+        while i < ERROR_TABLE.len() {
+            let (n, e, _) = ERROR_TABLE[i];
+            if n == code {
+                return e;
+            }
+            i += 1;
+        }
+        None
     }
 
     /// IchigoJam 標準のメッセージ文言。
     pub const fn message(self) -> &'static str {
-        match self {
-            BasicError::SyntaxError => "Syntax error",
-            BasicError::OutOfMemory => "Out of memory",
-            BasicError::StackOverflow => "Stack overflow",
-            BasicError::NotMatch => "Not match",
-            BasicError::UndefinedLine => "Line error",
-            BasicError::DivideByZero => "Divide by 0",
-            BasicError::IndexOutOfRange => "Index out of range",
-            BasicError::FileError => "File error",
-            BasicError::SegmentationFault => "Segmentation Fault",
-            BasicError::ComplexExpression => "Complex expression",
-            BasicError::IllegalArgument => "Illegal argument",
-            BasicError::Break => "Break",
-        }
+        message_for_code(self.code())
     }
+}
+
+/// `Machine.err` (= 数値コード) からメッセージ文言を引く。
+/// 範囲外の数値は空文字列を返す (元 C 実装と同じ挙動)。
+pub(crate) const fn message_for_code(code: u8) -> &'static str {
+    let mut i = 0;
+    while i < ERROR_TABLE.len() {
+        let (n, _, msg) = ERROR_TABLE[i];
+        if n == code {
+            return msg;
+        }
+        i += 1;
+    }
+    ""
+}
+
+/// const 関数で `BasicError` 同士を比較するためのヘルパ
+/// (列挙型に `Eq` を `derive` していても `==` は const 不可)。
+const fn matches_variant(a: BasicError, b: BasicError) -> bool {
+    a as u8 == b as u8
 }
 
 impl fmt::Display for BasicError {
@@ -107,21 +126,3 @@ pub(crate) const ERR_INDEX_OUT_OF_RANGE: u8 = BasicError::IndexOutOfRange.code()
 pub(crate) const ERR_FILE_ERROR: u8 = BasicError::FileError.code();
 pub(crate) const ERR_ILLEGAL_ARGUMENT: u8 = BasicError::IllegalArgument.code();
 pub(crate) const ERR_BREAK: u8 = BasicError::Break.code();
-
-/// `basic_print_error` で添字参照されているメッセージ表。
-/// `BasicError::from_code(i)?.message()` と同等の内容。
-pub(crate) const ERR_MESSAGES: &[&str] = &[
-    "",
-    "Syntax error",
-    "Out of memory",
-    "Stack overflow",
-    "Not match",
-    "Line error",
-    "Divide by 0",
-    "Index out of range",
-    "File error",
-    "Segmentation Fault",
-    "Complex expression",
-    "Illegal argument",
-    "Break",
-];
